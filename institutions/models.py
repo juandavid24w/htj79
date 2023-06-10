3d4ae8f40c4247d074f77c1f1515641353a06728
Juan0402 <davidbare0402@gmail.com>
2023-06-10 15:58:20 +0530

    Added Institution Models

--- institutions/models.py
+++ institutions/models.py
@@ -1,7 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from uuid import uuid4
 
 # Create your models here.
 
 
 class Institutions(models.Model):
-    pass
+    id = models.UUIDField(primary_key=True,
+                          default=uuid4,
+                          editable=False,
+                          verbose_name='id')
+    name = models.CharField(max_length=256, verbose_name=_('Institute Name'))
+    # coordinaters
+    address = models.TextField(verbose_name=_('Address'),
+                               help_text=_('Address of The Institute'))
+    location = models.ForeignKey('hacktivist.Locations',
+                                 on_delete=models.RESTRICT,
+                                 verbose_name=_('Location'),
+                                 help_text=_('Pick your Institute location'))
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      auto_created=True,
+                                      null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} | {self.location.district}'
+
+    class Meta:
+        ordering = ['name', '-updated_at']
+        verbose_name = _('Institution')
+        verbose_name_plural = _('Institutions')
