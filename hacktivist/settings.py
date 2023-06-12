8864488341df2d753c75208c61eee2f9560406f7
Juan0402 <davidbare0402@gmail.com>
2023-06-12 12:30:12 +0530

    Added static and media root

--- hacktivist/settings.py
+++ hacktivist/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = Path(BASE_DIR / 'templates')
+STATIC_DIR = Path(BASE_DIR / 'static')
 MEDIA_DIR = Path(BASE_DIR / 'media')
 
 # Quick-start development settings - unsuitable for production
@@ -124,11 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = Path(BASE_DIR / 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # MEDIA_ROOT
-MEDIA_URL = "/media/"
 MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = "hacktivist/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
