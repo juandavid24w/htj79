e14c5f320beed1921b055b919c85e643aa181bdd
Juan0402 <davidbare0402@gmail.com>
2023-08-17 18:06:47 +0530

    WIP

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from hacktivist.models import Platform
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
+class Meetups(models.Model):
+    location = models.ForeignKey('hacktivist.Locations', on_delete= models.CASCADE)
+    glug = models.ForeignKey('glug.GLUG', on_delete=models.RESTRICT)
+    date = models.DateField()
+    time = models.TimeField()
+    platform = models.CharField(choices=Platform.choices)
+    venue = models.TextField()
+    description = models.TextField()
+    minutes = models.FileField(
+        upload_to='meetups/minutes/',
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=['pdf'])
+        ])
+    poster = models.ImageField(verbose_name=_('Poster'),
+                                    upload_to="meetups/poster/")
