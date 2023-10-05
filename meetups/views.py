3d18297b484745803086c48cf642d28a708bc27d
Juan0402 <davidbare0402@gmail.com>
2023-10-06 03:58:57 +0530

    Work in progress

--- meetups/views.py
+++ meetups/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render, resolve_url, redirect
 from meetups.forms import *
 
 # Create your views here.
-def demo(request):
+def events(request):
     if request.user.is_authenticated:
-        return render(request, template_name='meetup_forms.html', context={'form': MeetupRegistrationForm})
+        return render(request, template_name='meetup_forms.html', context={'form': MeetupForm})
     else:
         return redirect(resolve_url('home'))
+
+def create_meetup(request):
+    if request.method == 'POST':
+        form = MeetupForm(request.POST, request.FILES) 
+        if form.is_valid():
+            form.save()
+            return redirect('meetup_list')  # Redirect to the list view after successful creation
+    else:
+        return render(request, 'create_meetup.html', {'form': MeetupForm})
