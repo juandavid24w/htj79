1dd6e2fc378b1f372dbc2c8f885d513520af6e37
Juan0402 <davidbare0402@gmail.com>
2023-10-06 03:55:52 +0530

    Updates forms

--- meetups/forms.py
+++ meetups/forms.py
@@ -1,14 +1,12 @@
 from django import forms
 from django.core import validators
 from glug.models import GLUG
+from hacktivist.models import Locations
+from meetups.models import Meetups
 
-class MeetupRegistrationForm(forms.ModelForm):
-    glug = forms.ModelChoiceField(
-        label='',
-        required=True,
-        queryset=GLUG.objects.all(),
-        widget=forms.Select(
-            attrs={
-                'class':
-                'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            }))
+class MeetupForm(forms.ModelForm):
+    location = forms.ModelChoiceField(queryset=Locations.objects.all())
+    glug = forms.ModelChoiceField(queryset=GLUG.objects.all())
+    class Meta:
+        model= Meetups
+        fields=['location', 'glug', 'date', 'time', 'platform', 'venue', 'description', 'minutes', 'poster']
