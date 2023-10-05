82e14b908e4aa553d23c692fa9a606bd769dac2d
Juan0402 <davidbare0402@gmail.com>
2023-10-06 01:48:28 +0530

    Added allauth

--- hacktivist/settings.py
+++ hacktivist/settings.py
@@ -57,12 +57,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "hacktivist.urls"
