9b59dfcfb5bd63a213198159a0cd5b5df622cba1
Juan0402 <davidbare0402@gmail.com>
2023-10-06 11:26:52 +0530

    Core - refactor occupation models
    Signed-off-by: Kamalavelan <sskamalavelan@gmail.com>

--- hacktivist/models.py
+++ hacktivist/models.py
@@ -6,24 +6,21 @@ from uuid import uuid4
 
 class Locations(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    district = models.CharField(max_length=256,
-                                verbose_name=_('district'),
-                                help_text=_(''))
-    state = models.CharField(max_length=256,
-                             verbose_name=_('state'),
-                             help_text=_(''))
-    country = models.CharField(max_length=256,
-                               verbose_name=_('country'),
-                               help_text=_(''),
-                               default='India')
-    location = models.PointField(geography=True,
-                                 verbose_name=_('location'),
-                                 help_text=_('point your GLUG location'),
-                                 null=True,
-                                 blank=True)
-    created_at = models.DateTimeField(auto_now_add=True,
-                                      auto_created=True,
-                                      null=True)
+    district = models.CharField(
+        max_length=256, verbose_name=_("district"), help_text=_("")
+    )
+    state = models.CharField(max_length=256, verbose_name=_("state"), help_text=_(""))
+    country = models.CharField(
+        max_length=256, verbose_name=_("country"), help_text=_(""), default="India"
+    )
+    location = models.PointField(
+        geography=True,
+        verbose_name=_("location"),
+        help_text=_("point your GLUG location"),
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True, auto_created=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     @property
@@ -41,83 +38,84 @@ class Locations(models.Model):
             return self.location.y
 
     def __str__(self):
-        return f'{self.district}'
+        return f"{self.district}"
 
     class Meta:
-        verbose_name = _('location')
-        verbose_name_plural = _('locations')
+        verbose_name = _("location")
+        verbose_name_plural = _("locations")
 
 
 # Text Choices
 class Gender(models.TextChoices):
-    man = 'Man', 'Man'
-    woman = 'Woman', 'Woman'
-    trans = 'Trans', 'Trans'
-    queer = 'Queer', 'Queer'
-    other = 'Other', 'Other'
+    man = "Man", "Man"
+    woman = "Woman", "Woman"
+    trans = "Trans", "Trans"
+    queer = "Queer", "Queer"
+    other = "Other", "Other"
 
 
 class BloodGroup(models.TextChoices):
-    op = 'O+ve', 'O+ve'
-    on = 'O-ve', 'O-ve'
-    ap = 'A+ve', 'A+ve'
-    an = 'A-ve', 'A-ve'
-    bp = 'B+ve', 'B+ve'
-    bn = 'B-ve', 'B-ve'
-    abp = 'AB+ve', 'AB+ve'
-    abn = 'AB-ve', 'AB-ve'
+    op = "O+ve", "O+ve"
+    on = "O-ve", "O-ve"
+    ap = "A+ve", "A+ve"
+    an = "A-ve", "A-ve"
+    bp = "B+ve", "B+ve"
+    bn = "B-ve", "B-ve"
+    abp = "AB+ve", "AB+ve"
+    abn = "AB-ve", "AB-ve"
 
 
 class Occupation(models.TextChoices):
-    s_student = 'School Student', 'School Student'
-    c_student = 'College/University Student', 'College/University Student'
-    researcher = 'Researcher', 'Researcher'
-    it_employee = 'IT Employee', 'IT Employee'
-    ites_employee = 'ITES Employee', 'ITES Employee'
-    non_it_ites_employee = 'Non IT/ITES Employee', 'Non IT/ITES Employee'
-    self_employed = 'Self Employed', 'Self Employed'
-    unemployed = 'Unemployed', 'Unemployed'
+    student_school = "School Student", "School Student"
+    student_college = "College/University Student", "College/University Student"
+    researcher = "Researcher", "Researcher"
+    employee_it = "IT Employee", "IT Employee"
+    employee_ites = "ITES Employee", "ITES Employee"
+    employee_others = "Non IT/ITES Employee", "Non IT/ITES Employee"
+    employee_self = "Self Employed", "Self Employed"
+    unemployed = "Unemployed", "Unemployed"
 
 
 class EducationalQualification(models.TextChoices):
-    eighth_pass = '8th Pass', '8th Pass'
-    tenth_pass = '10th Pass', '10th Pass'
-    twelveth_pass = '12th Pass', '12th Pass'
-    diploma = 'Diploma', 'Diploma'
-    under_graduate = 'Under Graduate', 'Under Graduate'
-    post_graduate = 'Post Graduate', 'Post Graduate'
-    doctorate = 'Doctorate', 'Doctorate'
+    eighth_pass = "8th Pass", "8th Pass"
+    tenth_pass = "10th Pass", "10th Pass"
+    twelfth_pass = "12th Pass", "12th Pass"
+    diploma = "Diploma", "Diploma"
+    under_graduate = "Under Graduate", "Under Graduate"
+    post_graduate = "Post Graduate", "Post Graduate"
+    doctorate = "Doctorate", "Doctorate"
 
 
 class Platform(models.TextChoices):
-    online = 'Online', 'Online'
-    offline = 'Offine', 'Offline'
+    online = "Online", "Online"
+    offline = "Offine", "Offline"
 
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=256,
-                            verbose_name=_('name'),
-                            help_text=_('Subscription name'))
+    name = models.CharField(
+        max_length=256, verbose_name=_("name"), help_text=_("Subscription name")
+    )
     validity = models.IntegerField(
-        verbose_name=_('Validity'),
-        help_text=_('Validity of subscription in years'),
-        default=1)
-    occupation = ArrayField(models.CharField(max_length=256,
-                                             choices=Occupation.choices),
-                            size=10,
-                            verbose_name=_('Occupation'),
-                            blank=True,
-                            null=True)
-    price = models.IntegerField(verbose_name=_('Price'),
-                                help_text=_('Price for the subscriptions'))
-    created_at = models.DateTimeField(auto_now_add=True,
-                                      auto_created=True,
-                                      null=True)
+        verbose_name=_("Validity"),
+        help_text=_("Validity of subscription in years"),
+        default=1,
+    )
+    occupation = ArrayField(
+        models.CharField(max_length=256, choices=Occupation.choices),
+        size=10,
+        verbose_name=_("Occupation"),
+        blank=True,
+        null=True,
+    )
+    price = models.IntegerField(
+        verbose_name=_("Price"), help_text=_("Price for the subscriptions")
+    )
+    created_at = models.DateTimeField(auto_now_add=True, auto_created=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.name} | {self.price}'
+        return f"{self.name} | {self.price}"
 
     class Meta:
-        verbose_name = _('subscription')
-        verbose_name_plural = _('subscriptions')
+        verbose_name = _("subscription")
+        verbose_name_plural = _("subscriptions")
