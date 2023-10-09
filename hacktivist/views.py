b288dbb094c3af79781544317121974f70a971f7
Juan0402 <davidbare0402@gmail.com>
2023-10-09 11:22:42 +0530

    Signup view disabled Completion check

--- hacktivist/views.py
+++ hacktivist/views.py
@@ -4,33 +4,24 @@ from member.forms import LoginForm
 
 
 def signin(request):
-    if request.user.is_authenticated:
-        if request.user.profile_status in ['4', '5'] or request.user.is_superuser:
-            return render(request, template_name='_base.html')
-        else:
-            return redirect(resolve_url('signup'))
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(resolve_url("home"))
+        return render(
+            request,
+            template_name="_base.html",
+            context={"form": LoginForm, "error": "email/password."},
+        )
     else:
-        if request.method == 'POST':
-            form = LoginForm(request.POST)
-            if form.is_valid():
-                email = form.cleaned_data.get('email')
-                password = form.cleaned_data.get('password')
-                user = authenticate(request, email=email, password=password)
-                if user is not None:
-                    login(request, user)
-                    return redirect(resolve_url('home'))
-            return render(request,
-                          template_name='_base.html',
-                          context={
-                              'form': LoginForm,
-                              'error': 'email/password.'
-                          })
-        else:
-            return render(request,
-                          template_name='_base.html',
-                          context={'form': LoginForm})
+        return render(request, template_name="_base.html", context={"form": LoginForm})
 
 
 def signout(request):
     logout(request)
-    return redirect(resolve_url('home'))
+    return redirect(resolve_url("home"))
