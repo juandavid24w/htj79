befface9d60cf48448411d797626dda3776f58c8
Juan0402 <davidbare0402@gmail.com>
2023-10-10 19:26:45 +0530

    Added errors condition

--- meetups/views.py
+++ meetups/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render, resolve_url, redirect
-from meetups.forms import *
+from meetups.forms import MeetupForm
+
 
 # Create your views here.
 def events(request):
     if request.user.is_authenticated:
-        return render(request, template_name='meetup_forms.html', context={'form': MeetupForm})
+        return render(
+            request, template_name="meetup_forms.html", context={"form": MeetupForm}
+        )
     else:
-        return redirect(resolve_url('home'))
+        return redirect(resolve_url("home"))
+
 
 def createMeetup(request):
-    if request.method == 'POST':
-        form = MeetupForm(request.POST, request.FILES) 
+    if request.method == "POST":
+        form = MeetupForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect,('meetups_list')  # Redirect to the list view after successful creation
+            return redirect(
+                resolve_url("meetups_list")
+            )  # Redirect to the list view after successful creation
+        else:
+            errors = form.errors
+            print(form.data)
+            return render(
+                request, "create_meetup.html", {"form": form, "errors": errors}
+            )
     else:
-        form= MeetupForm
-        return render(request, 'create_meetup.html', {'form': form})
+        return render(request, "create_meetup.html", {"form": MeetupForm})
