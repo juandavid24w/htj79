89403444467848c5b3e749acf739e3c203a58cfb
Juan0402 <davidbare0402@gmail.com>
2023-10-11 00:16:54 +0530

    removed unwanted safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,7 @@ module.exports = {
     "./*/forms.py",
     "./node_modules/flowbite/**/*.js",
   ],
-  safelist: [
-    "w-64",
-    "w-1/2",
-    "rounded-l-lg",
-    "rounded-r-lg",
-    "bg-gray-200",
-    "grid-cols-4",
-    "grid-cols-7",
-    "h-6",
-    "leading-6",
-    "h-9",
-    "leading-9",
-    "shadow-lg",
-    "errorlist",
-    "errortext",
-  ],
+  safelist: ["errorlist", "errortext"],
   darkMode: "class",
   theme: {
     fontFamily: {
