f17be4e93f6e9961feb9d5578e0b0f026c3a0b81
Juan0402 <davidbare0402@gmail.com>
2023-10-10 19:25:51 +0530

    Added widget for time input

--- meetups/forms.py
+++ meetups/forms.py
@@ -4,32 +4,48 @@ from django.core import validators
 from glug.models import GLUG
 from hacktivist.models import Locations
 from meetups.models import Meetups
+
+
 class MeetupForm(forms.ModelForm):
     location = forms.ModelChoiceField(
         queryset=Locations.objects.all(),
         widget=forms.Select(
-                attrs={
-                    "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                })
-        
+            attrs={
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            }
+        ),
     )
     glug = forms.ModelChoiceField(
-            label="",
-            required=True,
-            queryset=GLUG.objects.all(),
-            widget=forms.Select(
-                attrs={
-                    "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                }
-            )
-        )
-    date = forms.DateField( 
-            widget=widgets.DateInput(attrs={'type': 'date', "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}),
-        )
-    time = forms.TimeField()
-    minutes= forms.TextInput(
-        attrs={"class":"text-white font-medium rounded-lg text-base px-5 py-3 w-full sm:w-auto text-center bg-blue-700"}
+        label="",
+        required=True,
+        queryset=GLUG.objects.all(),
+        widget=forms.Select(
+            attrs={
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            }
+        ),
     )
+    date = forms.DateField(
+        widget=widgets.DateInput(
+            attrs={
+                "type": "date",
+                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+            }
+        ),
+    )
+    time = forms.TimeField(widget=forms.TimeInput(format="%H:%M"))
+    minutes = forms.FileField()
+
     class Meta:
-        model= Meetups
-        fields=['location','glug', 'date', 'time', 'mode', 'venue', 'description', 'minutes', 'poster']
+        model = Meetups
+        fields = [
+            "location",
+            "glug",
+            "date",
+            "time",
+            "mode",
+            "venue",
+            "description",
+            "minutes",
+            "poster",
+        ]
