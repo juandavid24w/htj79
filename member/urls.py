aa49178dd0525cbe4c38ccffa5a2c94407d1ce49
Juan0402 <davidbare0402@gmail.com>
2023-10-11 02:22:13 +0530

    userCreationView

--- member/urls.py
+++ member/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from member import views
 
 urlpatterns = [
-    path('signup/', views.userCreation, name='signup'),
+    path("signup/", views.UserCreationView.as_view(), name="signup"),
 ]
--- member/views.py
+++ member/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
+from django.views import View
+from django.http import HttpResponse
+from django.views.decorators.http import require_GET, require_POST, require_safe
 from member.forms import (
     UserCreationForm,
 )
 
 
-@require_http_methods(["GET", "POST"])
-def userCreation(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
+class UserCreationView(View):
+    form_class = UserCreationForm
+    template_name = "signup/userCreate.html"
+
+    def get(self, request):
+        return render(
+            request,
+            template_name=self.template_name,
+            context={"form": self.form_class},
+        )
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             obj = form.save(commit=False)
             obj.password = make_password(form.data.get("password"))
@@ -21,15 +32,12 @@ def userCreation(request):
         else:
             return render(
                 request,
-                template_name="signup/step1.html",
+                template_name=self.template_name,
                 context={
                     "form": form,
                     "errors": form.errors,
                 },
             )
-    else:
-        return render(
-            request,
-            template_name="signup/step1.html",
-            context={"form": UserCreationForm},
-        )
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        return HttpResponse("method is not allowed", status=405)
