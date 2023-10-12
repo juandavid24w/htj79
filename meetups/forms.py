ddc214fae02969829e55b373a38a3defe63b5125
Juan0402 <davidbare0402@gmail.com>
2023-10-12 10:21:20 +0530

    removed attribs,class

--- meetups/forms.py
+++ meetups/forms.py
@@ -9,29 +9,16 @@ from meetups.models import Meetups
 class MeetupForm(forms.ModelForm):
     location = forms.ModelChoiceField(
         queryset=Locations.objects.all(),
-        widget=forms.Select(
-            attrs={
-                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-        ),
+        widget=forms.Select(),
     )
     glug = forms.ModelChoiceField(
         label="",
         required=True,
         queryset=GLUG.objects.all(),
-        widget=forms.Select(
-            attrs={
-                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            }
-        ),
+        widget=forms.Select(),
     )
     date = forms.DateField(
-        widget=widgets.DateInput(
-            attrs={
-                "type": "date",
-                "class": "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-            }
-        ),
+        widget=widgets.DateInput(attrs={"type": "date"}),
     )
     time = forms.TimeField(widget=forms.TimeInput(format="%H:%M"))
     poster = forms.ImageField()
