1a36f1bfc478ed84bffafebb5aea06c5d4548677
Juan0402 <davidbare0402@gmail.com>
2023-10-13 03:21:58 +0530

    Added minutes

--- meetups/models.py
+++ meetups/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from hacktivist.models import Platform
 from django.core.validators import FileExtensionValidator
 from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
+from django.conf import settings
+from hacktivist.models import Locations
+from glug.models import GLUG
 
 
 # Create your models here.
 class Meetups(models.Model):
+    # owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     location = models.ForeignKey("hacktivist.Locations", on_delete=models.CASCADE)
     glug = models.ForeignKey("glug.GLUG", on_delete=models.RESTRICT)
     date = models.DateField()
@@ -14,6 +19,7 @@ class Meetups(models.Model):
     venue = models.TextField()
     description = models.TextField()
     poster = models.ImageField(verbose_name=_("Poster"), upload_to="meetups/poster/")
+    minutes = models.FileField(validators=[FileExtensionValidator(["pdf"])])
 
     def __str__(self):
         return f"{self.glug} ({self.location})"
