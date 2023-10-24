4c4109f62578603f7051d7df33443f71f2345cf3
Juan0402 <davidbare0402@gmail.com>
2023-10-24 11:49:43 +0530

    Add members model to Admin
    Signed-off-by: Kamalavelan <sskamalavelan@gmail.com>

--- member/admin.py
+++ member/admin.py
@@ -1,35 +1,40 @@
 from django.contrib import admin
-from member.models import Membership, ProofOfPayment
+from member.models import Membership, ProofOfPayment, Members
 
 # Register your models here.
 
 
 class MembershipAdmin(admin.ModelAdmin):
     list_display = [
-        'member',
-        'join_date',
-        'expiry_date',
+        "member",
+        "join_date",
+        "expiry_date",
     ]
     readonly_fields = [
-        'created_at',
-        'updated_at',
+        "created_at",
+        "updated_at",
     ]
 
 
 class ProofOfPaymentAdmin(admin.ModelAdmin):
     list_display = [
-        'transaction_id',
-        'is_verified',
+        "transaction_id",
+        "is_verified",
     ]
     readonly_fields = [
-        'transaction_id',
-        'document',
-        'is_verified',
-        'verified_by',
-        'created_at',
-        'updated_at',
+        "transaction_id",
+        "document",
+        "is_verified",
+        "verified_by",
+        "created_at",
+        "updated_at",
     ]
+
+
+class MembersAdmin(admin.ModelAdmin):
+    pass
 
 
 admin.site.register(ProofOfPayment, ProofOfPaymentAdmin)
 admin.site.register(Membership, MembershipAdmin)
+admin.site.register(Members, MembersAdmin)
