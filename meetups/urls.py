53100d8d04d5e871efd8c43c5fe3753d6a1e26a2
Juan0402 <davidbare0402@gmail.com>
2023-10-30 17:36:46 +0530

    Meetup details page linked

--- meetups/urls.py
+++ meetups/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("list/", views.events, name="meetups_list"),
     path("create_meetups/", views.MeetupCreationView.as_view(), name="meetups_create"),
     path("edit/<int:meetup_id>/", views.MeetupEditView.as_view(), name="meetups_edit"),
+    path("details/<int:meetup_id>/", views.MeetupDetails, name="meetup_details"),
 ]
