d22a28ef8fd16c7dab376b90bf626485938b976f
Juan0402 <davidbare0402@gmail.com>
2023-11-07 00:31:10 +0530

    Meetup urls to list all will be /meetups

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from meetups import views
 
 urlpatterns = [
-    path("list/", views.events, name="meetups_list"),
+    path("", views.events, name="meetups_list"),
     path("create_meetups/", views.MeetupCreationView.as_view(), name="meetups_create"),
     path("edit/<int:meetup_id>/", views.MeetupEditView.as_view(), name="meetups_edit"),
     path("details/<int:meetup_id>/", views.MeetupDetailView.as_view(), name="meetup_details"),
