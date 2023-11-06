df3f842fc172453588b7adb38c72a05936a1d19f
Juan0402 <davidbare0402@gmail.com>
2023-11-07 00:40:52 +0530

    Added slug in url pathparams

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,7 +4,7 @@ from meetups import views
 urlpatterns = [
     path("", views.events, name="meetups_list"),
     path("create_meetups/", views.MeetupCreationView.as_view(), name="meetups_create"),
-    path("edit/<int:meetup_id>/", views.MeetupEditView.as_view(), name="meetups_edit"),
-    path("details/<int:meetup_id>/", views.MeetupDetailView.as_view(), name="meetup_details"),
+    path("edit/<str:slug>/", views.MeetupEditView.as_view(), name="meetups_edit"),
+    path("details/<str:slug>/", views.MeetupDetailView.as_view(), name="meetup_details"),
 ]
 
