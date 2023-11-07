8ce8b552b8d1f68aef9e4b83b6a4af6ba63bb48d
Juan0402 <davidbare0402@gmail.com>
2023-11-07 23:56:50 +0530

    Imported slugify and defined function to create unique for every meetup

--- meetups/models.py
+++ meetups/models.py
@@ -4,6 +4,7 @@ from django.core.validators import FileExtensionValidator
 from django.utils.translation import gettext as _
 from django.conf import settings
 import uuid
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -25,6 +26,11 @@ class Meetups(models.Model):
         verbose_name=_("Poster"), upload_to="meetups/poster/", null=True
     )
     minutes = models.FileField(validators=[FileExtensionValidator(["pdf"])], null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Meetups, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.glug} ({self.location})"
