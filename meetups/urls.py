435708771daea92914d02ed8c4e0fadec78e7f3a
Juan0402 <davidbare0402@gmail.com>
2023-11-14 12:41:01 +0530

    Updated URL

--- meetups/urls.py
+++ meetups/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("create_meetups/", views.MeetupCreationView.as_view(), name="meetups_create"),
     path("edit/<slug:meetup_id>/", views.MeetupEditView.as_view(), name="meetups_edit"),
     path(
-        "details/<slug:meetup_id>/",
+        "details/<slug:slug>/",
         views.MeetupDetailView.as_view(),
         name="meetup_details",
     ),
