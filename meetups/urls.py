6b23c3a6515fb77cb19977bd3bce38483cd7b70f
Juan0402 <davidbare0402@gmail.com>
2023-11-14 12:25:01 +0530

    URL changed

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,7 +4,10 @@ from meetups import views
 urlpatterns = [
     path("", views.events, name="meetups_list"),
     path("create_meetups/", views.MeetupCreationView.as_view(), name="meetups_create"),
-    path("edit/<slug:slug>/", views.MeetupEditView.as_view(), name="meetups_edit"),
-    path("details/<slug:slug>/", views.MeetupDetailView.as_view(), name="meetup_details"),
+    path("edit/<slug:meetup_id>/", views.MeetupEditView.as_view(), name="meetups_edit"),
+    path(
+        "details/<slug:meetup_id>/",
+        views.MeetupDetailView.as_view(),
+        name="meetup_details",
+    ),
 ]
-
